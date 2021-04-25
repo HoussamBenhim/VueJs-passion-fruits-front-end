e3ca13f0789a34e079bdc80bda380bb8c4b28e50
Houssam Benhim <benhim.houssam@gmail.com>
2021-04-25 20:32:04 +0200

    Tailwind Upgrade + backgroundVideo

--- components/BackgroundVideo/DisplayBox.global.vue
+++ components/BackgroundVideo/DisplayBox.global.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="w-full h-48 relative">
-    <video
-      class="w-full h-full"
-      preload="auto"
-      autoplay="true"
-      loop="true"
-      src="https://static.frichti.co/assets/videos/hypermarket.mp4"
-      type="video/mp4"
-    >
-      <source
+  <div class="relative">
+    <div class="inset-0">
+      <video
+        class="object-cover w-full h-50"
+        preload="auto"
+        autoplay="true"
+        loop="true"
         src="https://static.frichti.co/assets/videos/hypermarket.mp4"
         type="video/mp4"
-      />
-    </video>
-
-    <div class="absolute -mt-16">
-      <p>Marché direct producteur</p>
-      <p>Epicerie 100% clean.</p>
+      ></video>
+    </div>
+    <div class="absolute inset-0 bg-gray-900 w-full bg-opacity-30"></div>
+    <div class="absolute top-1/3 w-full font-sans italic">
+      <h2
+        class="text-4xl md:text-5xl text-center text-white tracking-wide leading-8 font-black"
+      >
+        Des plats délicieux
+      </h2>
+      <h2
+        class="text-4xl md:text-5xl text-center text-white leading-tight font-black text-shadow-lg"
+      >
+        en direct de nos cuisine.
+      </h2>
     </div>
   </div>
 </template>
